2b82fe0a73586cff57a96c4a3488de4d5ef2e436
gerbil1511 <gerbil1511@gmail.com>
2025-10-06 11:26:48 +0100

    update agenda dummy objects to include revision dates and userID

--- agenda.mjs
+++ agenda.mjs
@@ -1,23 +1,63 @@
 // object contains key of topic and date
-export const agenda = [
+export const agendas = [
   {
+    userId: "1",
     topic: "Introduction to Spaced Repetition",
-    date: "2024-07-01",
+    initialDate: "2025-07-01",
+    revisionDates: [
+      "2025-07-08",
+      "2025-08-01",
+      "2025-11-01",
+      "2026-02-01",
+      "2026-07-01",
+    ],
   },
   {
+    userId: "2",
     topic: "Introduction to objects and arrays",
-    date: "2024-07-02",
+    initialDate: "2025-07-02",
+    revisionDates: [
+      "2025-07-09",
+      "2025-08-02",
+      "2025-11-02",
+      "2026-02-02",
+      "2026-07-02",
+    ],
   },
   {
+    userId: "3",
     topic: "What is the DOM?",
-    date: "2024-07-03",
+    initialDate: "2025-07-03",
+    revisionDates: [
+      "2025-07-10",
+      "2025-08-03",
+      "2025-11-03",
+      "2026-02-03",
+      "2026-07-03",
+    ],
   },
   {
+    userId: "4",
     topic: "How to fetch data from an API",
-    date: "2024-07-04",
+    initialDate: "2025-07-04",
+    revisionDates: [
+      "2025-07-11",
+      "2025-08-04",
+      "2025-11-04",
+      "2026-02-04",
+      "2026-07-04",
+    ],
   },
   {
+    userId: "5",
     topic: "Introduction to Local Storage",
-    date: "2024-07-05",
+    initialDate: "2024-11-05",
+    revisionDates: [
+      "2024-11-12",
+      "2024-12-05",
+      "2025-03-05",
+      "2025-06-05",
+      "2025-11-05",
+    ],
   },
 ];
